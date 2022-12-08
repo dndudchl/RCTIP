847dc893c69607b52a4cdd1a48ad240c7e1e0fed
dndudchl <flvmjf32@gmail.com>
2022-12-08 11:11:17 +0900

    ttps

--- src/app/modules/RaaS/TTPs/components/Hive.tsx
+++ src/app/modules/RaaS/TTPs/components/Hive.tsx
@@ -7,42 +7,40 @@ type Propx = {
 
 const Hive: React.FC<Propx> = ({className}) => {  
   return (
+	
     <div className={`card ${className}`}>
       <div className='card-header border-0 pt-5'>
-        <h2 className='card-title fw-bold fs-1 text-center'>MITRE ATT&CK MATRIX
-        </h2>
+        <h1 className='card-title fw-bold fs-2qx'>MITRE ATT&CK MATRIX
+        </h1>
       </div>
-      <div className='card-body py-3'>
+      <div className='card-body pt-3 pb-6'>
         <div className='table-responsive border-start border-end border-white'>
-          <table className='table table-fixed align-baseline gx-1 gy-4 table-success table-striped border border-dark'>
+          <table className='table align-baseline gx-1 gy-4 table-striped border border-dark'>
             <thead>
-              <tr className='fw-bold text-white bg-dark text-center table-fixed'>
-               <th className='border-start border-dark min-w-125px'>Initial-Access</th>
-               <th className='border min-w-125px'>Execution</th>
-               <th className='border min-w-125px'>Persistence</th>
-               <th className='border min-w-125px'>Privilege-Escalation</th>
-               <th className='border min-w-125px'>Defense-Evasion</th>
-               <th className='border min-w-125px'>Credential-Access</th>
-               <th className='border min-w-125px'>Discovery</th>
-               <th className='border min-w-125px'>Lateral-Movement</th>
-               <th className='border min-w-125px'>Command-and-Control</th>
-               <th className='border-end border-dark min-w-125px'>Impact</th>
+              <tr className='fs-6 fw-semebold text-white bg-dark text-center'>
+               <th className='border-start border-dark min-w-175px mw-185px'>Initial-Access</th>
+               <th className='border min-w-175px'>Execution</th>
+               <th className='border min-w-175px'>Persistence</th>
+               <th className='border min-w-175px'>Privilege-Escalation</th>
+               <th className='border min-w-175px'>Defense-Evasion</th>
+               <th className='border min-w-175px'>Credential-Access</th>
+               <th className='border min-w-175px'>Discovery</th>
+               <th className='border min-w-175px'>Lateral-Movement</th>
+               <th className='border min-w-175px'>Command-and-Control</th>
+               <th className='border-end border-dark min-w-175px'>Impact</th>
               </tr>
             </thead>
             <tbody>
 							<tr className=''>              
 			   					<td className='border-start border-end border-dark'>
-									<div className='px-2 d-flex align-items-baseline'>
-										<div className='symbol symbol-50px me-2'>
-										<span className='badge badge-secondary fs-8 fw-bold '>T1190</span>
+									<div className='px-2 d-flex flex-column'>
+										<div className='symbol symbol-50px'>
+										<span className='badge badge-dark text-white fs-8 fw-bold me-1'>T1190</span>
+										<span className='text-dark fw-semibold mb-1 fs-7'>- Exploit Public-Facing Application</span>
 										</div>
-										<div className='d-flex justify-content-baseline flex-column'>
-											<div className='text-dark fw-semibold mb-1 fs-7'>
-												- Exploit Public-Facing Application
-											</div>
-											<div className='text-muted fw-semibold text-muted d-block fs-7'>
-
-											</div>
+										{/* <div className='d-flex justify-content-baseline flex-column'> */}
+										<div className='bg-gray-500 border-start border-danger1 border-3 ps-4 pt-4 pb-1 '>
+											<div className='text-white fw-semibold d-block fs-7'> hihi hihihihi hihihihi hihihihi hihihihi </div>
 										</div>
 									</div>
 								</td>
